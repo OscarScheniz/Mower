d057aebcd867eebb26d172eae8b200703f5d91f9
Oscar <scos1720@student.ju.se>
2020-04-26 12:14:13 +0200

    add manual drive and init

--- mower_main/mower_main.ino
+++ mower_main/mower_main.ino
@@ -8,6 +8,7 @@
 #define IR_SENSOR         0x02
 #define MANUAL_MODE       0x03
 #define AUTONOM_MODE      0x04
+#define VELOCITY          20    //20 CM/s 
 
 #define BUFF_LEN          6
 #define MAX_MILLIS_TO_WAIT 300  
@@ -19,9 +20,12 @@ MeEncoderOnBoard Encoder_2(SLOT2);
 MeEncoderMotor encoders[2];
 
 uint8_t irRead = 0;
-int16_t moveSpeed = 180;
+int16_t moveSpeed = 127;
+long timer = 0;
+long prev_timer= 0;
+int prev_cmd = 1; //start in forward
 
-byte arr[BUFF_LEN] = {0x00, 0x00, 0x00, 0x7F, 0x7F, 0x00};
+byte arr[BUFF_LEN] = {0};
 byte rxArr[BUFF_LEN] = {0};
 
 void encoder1_process(void)
@@ -92,11 +96,25 @@ void BackwardAndTurnRight(){
   Stop();
 }
 
+void init(byte arr[]){
+
+  arr[0] = 0x00;
+  arr[1] = 0x00;
+  arr[2] = 0x01;
+  arr[3] = 0x7F;
+  arr[4] = 0x7F;
+  arr[5] = 0x00;
+
+  //timer = millis();
+  //attachInterrupt(arr[2], callback_func, CHANGE);
+  return;
+}
+
 void Ultrasonic_Process(int distance_cm)
 {
   if ((distance_cm < 20)){
-    arr[5] = 1; //Set collision true
-    bluetoothTransmit(arr);
+    setCollision(1);
+    //bluetoothTransmit(arr);
     int randnum = random (10);
     Serial.println(randnum);
     if (randnum < 5){
@@ -106,7 +124,8 @@ void Ultrasonic_Process(int distance_cm)
        BackwardAndTurnRight();
     }
   }
-   Forward();
+  setCollision(0);
+  Forward();
 }
 
 void Ir_Process(int value){
@@ -115,22 +134,30 @@ void Ir_Process(int value){
     
     case S1_IN_S2_IN: //
     //Serial.println("Sensor 1 and 2 are inside of black line"); //Backward
+    setCollision(1);
     BackwardAndTurnRight();
+    setCollision(0);
     break;
     
     case S1_IN_S2_OUT: //
     //Serial.println("Sensor 2 is outside of black line"); //Turn left
+    setCollision(1);
     turnLeft();
+    setCollision(0);
     break;
     
     case S1_OUT_S2_IN: //
     //Serial.println("Sensor 1 is outside of black line"); //Turn right
+    setCollision(1);
     turnRight();
+    setCollision(0);
     break;
     
     case S1_OUT_S2_OUT: //
     //Serial.println("Sensor 1 and 2 are outside of black line"); //Forward
+    setCollision(1);
     Forward();
+    setCollision(0);
     break;
   }
 }
@@ -179,7 +206,7 @@ void dismantleRX(byte arr[])
 void bluetoothTransmit(byte *arr)
 {
   Serial.write(arr, BUFF_LEN);
-  delay(1000);
+  delay(100);
 }
 
 void manualDrive(int arr)
@@ -242,15 +269,39 @@ void setLeftbit(){
 void setCollision(int i){
   arr[6] = i;
 }
+/*
+void calcMovementDistX(){
+  
+     int dataPointX = 127 + (VELOCITY * ((timer - prevTimer) * 1000)))/2; 
+}
+
+void calcMovementDistY(){
+  
+     int dataPointY = 127 + (VELOCITY * ((timer - prevTimer) * 1000)))/2; 
+}
+
+void callback_func()
+{
+  calcMovementDist(
+}*/
 
 void setup() {
   Serial.begin(115200);
+  init(arr);
   Forward();
 }
 
 void loop() {
-    
+
   bluetoothTransmit(arr);
+  /*if(prev_cmd != arr[2])
+  {
+    //Reset timer
+  }*/
+
+  //prev_cmd = arr[2];
+
+  
   
   dismantleRX(rxArr);
   if(rxArr[1] == 1) //Manual mode
